e0a99dd6c5b5e6604302ea1ed37e065b68085b32
IT25101540 <it25101540@my.sliit.lk>
2026-05-16 21:12:05 +0530

    Add backend pom.xml and basic configs

---
+++ backend/src/main/java/com/movieplatform/MoviePlatformApplication.java
@@ -0,0 +1,12 @@
+// SE1020 – Movie Platform SOA – Spring Boot REST API
+package com.movieplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MoviePlatformApplication.class, args);
+    }
+}
